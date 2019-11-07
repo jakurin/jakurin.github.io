43b939c8fae87198266525ce4ecfbea469eeae7f
Jakk Bm <jakk@Jakks-MacBook-Pro.local>
2019-11-08 00:27:29 +0100

    yandex map added

--- js/main.js
+++ js/main.js
@@ -5,3 +5,12 @@ $(document).ready(function () {
   });
 
 });
+
+ymaps.ready(init);
+	
+function init(){
+    var myMap = new ymaps.Map("map", {
+        center: [47.4979, 19.0402],
+        zoom: 13
+    });
+}
